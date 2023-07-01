ce361c112852e80570629ed6458ef799bc64ec3f
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-07-01 13:17:27 +0700

    Navigate Back Explicitly

--- app/src/main/java/com/rujirakongsomran/jc_navigationbasics/DetailScreen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_navigationbasics/DetailScreen.kt
@@ -23,7 +23,11 @@ fun DetailScreen(
     ) {
         Text(
             modifier = Modifier.clickable {
-                navController.popBackStack()
+                navController.navigate(Screen.Home.route){
+                    popUpTo(Screen.Home.route){
+                        inclusive = true
+                    }
+                }
             },
             text = "Detail",
             color = Color.Red,
