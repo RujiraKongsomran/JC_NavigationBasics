9ae1bd07d25eb8d68ee8297e1ad84803c7cdd787
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-07-05 19:51:16 +0700

    Single required argument

--- app/src/main/java/com/rujirakongsomran/jc_navigationbasics/NavGraph.kt
+++ app/src/main/java/com/rujirakongsomran/jc_navigationbasics/NavGraph.kt
@@ -1,12 +1,16 @@
 package com.rujirakongsomran.jc_navigationbasics
 
 import androidx.compose.runtime.Composable
+import androidx.media3.common.util.Log
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 
+@androidx.media3.common.util.UnstableApi
 @Composable
-fun SetupNavGraph(
+fun  SetupNavGraph(
     navHostController: NavHostController
 ) {
     NavHost(
@@ -15,12 +19,17 @@ fun SetupNavGraph(
     ) {
         composable(
             route = Screen.Home.route
-        ){
+        ) {
             HomeScreen(navController = navHostController)
         }
         composable(
-            route = Screen.Detail.route
-        ){
+            route = Screen.Detail.route,
+            arguments = listOf(navArgument(DETAIL_ARGUMENT_KEY) {
+                type = NavType.IntType
+            })
+
+        ) {
+            Log.d("Args", it.arguments?.getInt("id").toString())
             DetailScreen(navController = navHostController)
         }
     }
--- app/src/main/java/com/rujirakongsomran/jc_navigationbasics/Screen.kt
+++ app/src/main/java/com/rujirakongsomran/jc_navigationbasics/Screen.kt
@@ -1,6 +1,15 @@
 package com.rujirakongsomran.jc_navigationbasics
 
+const val DETAIL_ARGUMENT_KEY = "id"
+
 sealed class Screen(val route: String) {
     object Home : Screen(route = "home_screen")
-    object Detail : Screen(route = "detail_screen")
+    object Detail : Screen(route = "detail_screen/{$DETAIL_ARGUMENT_KEY}") {
+        fun passId(id: Int): String {
+            return this.route.replace(
+                oldValue = "{$DETAIL_ARGUMENT_KEY}",
+                newValue = id.toString()
+            )
+        }
+    }
 }
